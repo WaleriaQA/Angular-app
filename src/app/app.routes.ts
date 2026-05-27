5ccfe86be4ebadc8e313ebe870cb8d7a0529dc38
Waleria QA <waleria86@mail.ru>
2026-05-27 21:19:39 +0300

    feat: update UI and app structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
+import { Layout } from './common-ui/layout/layout';
 import { LoginPageComponent } from './pages/login-page/login-page';
+import { ProfilePageComponent } from './pages/profile-page/profile-page';
 import { SearchPageComponent } from './pages/search-page/search-page';
 
 export const routes: Routes = [
-    {path: '', component: SearchPageComponent},
+    {path: '', component: Layout, children: [
+        {path: '', component: SearchPageComponent},
+        {path: 'profile', component: ProfilePageComponent},
+    ]},
+    
     {path: 'login', component: LoginPageComponent}
 ];
 
--- src/app/pages/login-page/login-page.ts
+++ src/app/pages/login-page/login-page.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss',
 })
-export class LoginPageComponent {}
+export class LoginPageComponent {
+
+  form = new FormGroup({  
+    username: new FormControl(''),
+    password: new FormControl('')
+  });
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
+}
 
