305f67da160236128577c8029a635f01e27a1fc0
Waleria QA <waleria86@mail.ru>
2026-05-31 14:56:20 +0300

    fix: login page password toggle and form behavior

--- src/app/pages/login-page/login-page.ts
+++ src/app/pages/login-page/login-page.ts
@@ -4,11 +4,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { inject } from '@angular/core';
 import { AuthService } from '../../auth/auth';
 import { Router } from '@angular/router';
+import { NgClass } from '@angular/common';
+import { signal } from '@angular/core';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgClass],
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss',
 })
@@ -17,14 +19,14 @@ export class LoginPageComponent {
   authService = inject(AuthService);
    router = inject(Router);
 
+   isPasswordVisible = signal<boolean>(false);
+
   form = new FormGroup({  
     username: new FormControl<string | null>(null, Validators.required),
     password: new FormControl<string | null>(null, Validators.required)
   });
 
   onSubmit() {
-    console.log(this.form.value)
-
     if (this.form.valid) {
       //@ts-ignore
       this.authService.login(this.form.value).subscribe (res => {
