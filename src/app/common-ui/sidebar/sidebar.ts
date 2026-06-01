1464473d3e6b55f8567b17f78037f87ebcc0b2ef
Waleria QA <waleria86@mail.ru>
2026-06-01 09:42:02 +0300

    feat: add password visibility toggle and svg icons

--- src/app/common-ui/sidebar/sidebar.ts
+++ src/app/common-ui/sidebar/sidebar.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SvgIcon } from "../svg-icon/svg-icon";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [SvgIcon],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
