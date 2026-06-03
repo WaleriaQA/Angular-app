2684cd67c3d3b846bf7117d0ce76a33e8de7dfbd
Waleria QA <waleria86@mail.ru>
2026-06-03 20:12:29 +0300

    feat: implement sidebar subscribers section
    - add subscriber card component
    - add Pageble interface
    - add profile state signal in ProfileService
    - load current user profile
    - add sidebar footer profile block
    - add new svg icons
    - update login page and styles

--- src/app/common-ui/layout/layout.ts
+++ src/app/common-ui/layout/layout.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from '../sidebar/sidebar';
-import { ProfileService } from '../../data/services/profile.service';
-import { inject } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
@@ -12,12 +10,6 @@ import { inject } from '@angular/core';
   styleUrl: './layout.scss',
 })
 export class Layout {
-  profileService = inject(ProfileService);
+  
 
-  ngOnInit() {
-    console.log('ngOnInit');
-    this.profileService.getMe().subscribe(val => {
-      console.log(val)
-    });
-  }
 }
--- src/app/common-ui/sidebar/sidebar.ts
+++ src/app/common-ui/sidebar/sidebar.ts
@@ -1,11 +1,50 @@
 import { Component } from '@angular/core';
 import { SvgIcon } from "../svg-icon/svg-icon";
+import { NgFor, AsyncPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { SubscriberCard } from "./subscriber-card/subscriber-card";
+import { ProfileService } from '../../data/services/profile.service';
+import { inject } from '@angular/core';
+import { Profile } from '../../data/interfaces/profile.interface';
+import { WritableSignal } from '@angular/core';
+import { Pageble } from '../../data/interfaces/pageble.interface';
+import { firstValueFrom, Observable } from 'rxjs';
+import { OnInit } from '@angular/core';
+import { ImgUrlPipe } from '../../helpers/pipes/img-url-pipe';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [SvgIcon],
+  imports: [SvgIcon, NgFor, RouterLink, SubscriberCard, AsyncPipe, ImgUrlPipe],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
-export class Sidebar {}
+export class Sidebar {
+
+  profileService: ProfileService = inject(ProfileService);
+
+  subscribers$: Observable<Profile[]> = this.profileService.getSubscribersShortList();
+  me = this.profileService.me;
+  menuItems = [{
+    label: 'Home',
+    icon: 'home',
+    link: '',
+  },
+  {
+    label: 'Chats',
+    icon: 'chats',
+    link: 'chats',
+  },
+  {
+    label: 'Search',
+    icon: 'search',
+    link: 'search',
+  },
+
+];
+
+ngOnInit() {
+  firstValueFrom(this.profileService.getMe())
+}
+}
+
